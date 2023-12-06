26df28eb0cf30a4b5e8ed3bb34a2723a05381056
Nathan Stehr <nstehr@gmail.com>
2023-12-05 21:18:10 -0500

    solved part 2

--- day1/main.go
+++ day1/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
-	"unicode"
+	"strings"
 )
 
 const (
@@ -14,6 +14,26 @@ const (
 )
 
 func main() {
+	numMap := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+		"1":     "1",
+		"2":     "2",
+		"3":     "3",
+		"4":     "4",
+		"5":     "5",
+		"6":     "6",
+		"7":     "7",
+		"8":     "8",
+		"9":     "9",
+	}
 	file, err := os.Open(input)
 	if err != nil {
 		log.Fatal(err)
@@ -25,24 +45,35 @@ func main() {
 	for scanner.Scan() {
 		text := scanner.Text()
 		first := ""
+		firstIdx := math.MaxInt
 		last := ""
+		lastIdx := 0
 		log.Println(text)
-		for _, c := range text {
-			// c will be the unicode
-			if unicode.IsNumber(c) {
-				num := fmt.Sprintf("%c", c)
-				if first == "" {
-					first = num
+		// more elegant ways to solve this, but this works
+		for k, v := range numMap {
+			idx := strings.Index(text, k)
+			endIdx := strings.LastIndex(text, k)
+
+			if idx > -1 {
+				if idx <= firstIdx {
+					firstIdx = idx
+					first = v
 				}
-				last = num
+			}
+			if endIdx > -1 {
+				if endIdx >= lastIdx {
+					lastIdx = endIdx
+					last = v
+				}
 			}
 		}
 		final, err := strconv.Atoi(first + last)
 		if err != nil {
 			log.Fatal(err)
 		}
+		log.Println(final)
 		sum = sum + final
 
 	}
-	log.Println(sum)
+	log.Printf("Sum is: %d", sum)
 }
